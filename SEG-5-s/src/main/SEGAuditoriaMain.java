c9521d30fdd7bcabe40a10327f3a32674bdf73c8
Jaime Alexis García Hinojosa <alekzgh@gmail.com>
2017-11-29 23:50:45 -0600

    <Creacion de GUI>
    Se comienzan a establecer las ventanas & el formato que se va a seguir

---
+++ SEG-5-s/src/main/SEGAuditoriaMain.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import vista.SplashScreen;
+
+/**
+ *
+ * @author garcihard
+ */
+public class SEGAuditoriaMain {
+    
+    public static void main(String[] args) {
+        new SplashScreen().setVisible(true);
+    }
+}
